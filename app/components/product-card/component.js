da51e4e34a574a340805b016dd8c691ae5a720e3
Marina Ferreira <marina.ferreira.dev@gmail.com>
2018-11-23 08:26:38 -0200

    [cart] Product card installments

--- app/components/product-card/component.js
+++ app/components/product-card/component.js
@@ -1,6 +1,22 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
   tagName: 'li',
-  classNames: ['product-card']
+  classNames: ['product-card'],
+
+  price: computed('product.price', function() {
+    return this.product.price.toFixed(2);
+  }),
+
+  installments: computed('product.price', 'product.installments', function() {
+    let fullPrice = this.product.price;
+    let currency = this.product.currencyFormat;
+    let installments = this.product.installments;
+    let price = (fullPrice / installments).toFixed(2);
+
+    if (installments === 0) return;
+
+    return `ou ${installments} x ${currency} ${price}`;
+  })
 });
