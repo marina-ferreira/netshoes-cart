55f1bafbe446b96a323fb2995e670e904b521b10
Marina Ferreira <marina.ferreira.dev@gmail.com>
2018-11-24 15:01:44 -0200

    [cart] Currency format helper in product card

--- app/helpers/format-currency.js
+++ app/helpers/format-currency.js
@@ -2,11 +2,11 @@ import { helper } from '@ember/component/helper';
 
 export function formatCurrency([format, value, className]) {
   let price = document.createElement('p');
-  price.classList.add(`${className}__price`);
+  price.classList.add(`${className}price`);
 
   price.innerHTML = `
-    <span class="${className}__currency">${format}</span>
-    <span class="${className}__value">${value.toFixed(2)}</span>
+    <span class="${className}currency">${format}</span>
+    <span class="${className}value">${value.toFixed(2)}</span>
   `;
 
   return price;
