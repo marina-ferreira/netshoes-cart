16ae9cd521158ac29b0a420a3ad15b93776b1e81
Marina Ferreira <marina.ferreira.dev@gmail.com>
2018-11-25 14:47:13 -0200

    [enhancements] Adds random image to product

--- app/components/product-card/component.js
+++ app/components/product-card/component.js
@@ -6,6 +6,13 @@ export default Component.extend({
   classNames: ['product-card'],
   cart: service('shopping-cart'),
 
+  didReceiveAttrs() {
+    let randomNumber = Math.floor(Math.random() * 9);
+    let imgSrc = `img/product-image-${randomNumber}.jpg`;
+
+    this.product.set('image', imgSrc);
+  },
+
   didInsertElement() {
     let className = 'product-card-available-sizes';
     let availableSizes = this.element.querySelector(`.${className}`);
