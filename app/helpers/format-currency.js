4df958bbca7ed05002cf287d1fd4b4e4c4eb307a
Marina Ferreira <marina.ferreira.dev@gmail.com>
2018-11-25 11:19:57 -0200

    [cart] Format currency helper test update

--- app/helpers/format-currency.js
+++ app/helpers/format-currency.js
@@ -1,12 +1,16 @@
 import { helper } from '@ember/component/helper';
 
-export function formatCurrency([value=0, currency='$', className='']) {
+export function formatCurrency(args) {
+  args.value = args.value || 0;
+  args.currency = args.currency || '$';
+  args.className = args.className || '';
+
   let price = document.createElement('p');
-  price.classList.add(`${className}price`);
+  price.classList.add(`${args.className}price`);
 
   price.innerHTML = `
-    <span class="${className}currency">${currency}</span>
-    <span class="${className}value">${value.toFixed(2)}</span>
+    <span class="${args.className}currency">${args.currency}</span>
+    <span class="${args.className}value">${args.value.toFixed(2)}</span>
   `;
 
   return price;
