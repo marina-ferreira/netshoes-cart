2ea702215a56c01c95a433251207d9fe64cebc5b
Marina Ferreira <marina.ferreira.dev@gmail.com>
2018-11-25 15:27:42 -0200

    [enhancements] Fly product to cart animation

--- app/components/product-card/component.js
+++ app/components/product-card/component.js
@@ -1,5 +1,6 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
+import { later } from '@ember/runloop';
 
 export default Component.extend({
   tagName: 'li',
@@ -25,6 +26,8 @@ export default Component.extend({
   actions: {
     addToCart(size) {
       this.cart.add(this.product, size);
+
+      this._flyImagetoCart();
     },
 
     displaySizes() {
@@ -33,5 +36,37 @@ export default Component.extend({
 
       availableSizes.classList.add(`${className}--visible`);
     }
+  },
+
+  // private
+
+  _flyImagetoCart() {
+    let cartIcon = document.querySelector('.cart-icon');
+    let flyingImg = document.createElement('img');
+
+    flyingImg.src = this.product.image;
+    flyingImg.classList.add('product-card__flying-image');
+    this.element.append(flyingImg);
+
+    let [posX, posY] = this._getElementsPosition(cartIcon, flyingImg);
+
+    flyingImg.style.transform = `translate(${posX}, ${posY})`;
+    flyingImg.style.width = 0;
+
+    later(() => this.element.removeChild(flyingImg), 1100);
+  },
+
+  _getElementsPosition(cartIcon, flyingImg) {
+    let cartIconPos = cartIcon.getBoundingClientRect();
+    let flyingImgPos = flyingImg.getBoundingClientRect();
+
+    let cartOffsetX = cartIconPos.x + cartIconPos.width / 2;
+    let productOffsetX = flyingImgPos.x + flyingImgPos.width / 2;
+    let cartOffsetY = cartIconPos.y + cartIconPos.height / 2;
+
+    let posX = `${cartOffsetX - productOffsetX}px`;
+    let posY = `${cartOffsetY - flyingImgPos.y}px`;
+
+    return [posX, posY];
   }
 });
