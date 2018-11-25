0071c631c3859be9f9b4574d8112f55e876e63ea
Marina Ferreira <marina.ferreira.dev@gmail.com>
2018-11-25 11:14:20 -0200

    [cart] Format installments  helper test

--- app/helpers/format-installments.js
+++ app/helpers/format-installments.js
@@ -1,8 +1,14 @@
 import { helper } from '@ember/component/helper';
 
-export function formatInstallments([totalPrice, installments, currency]) {
-  let price = (totalPrice / installments).toFixed(2);
-  return `ou em até ${installments} x ${currency} ${price}`;
+export function formatInstallments(args) {
+  args.totalPrice = args.totalPrice || 0;
+  args.installments = args.installments || 0;
+  args.currency = args.currency || '$';
+
+  let price = (args.totalPrice / args.installments).toFixed(2);
+  if (price === 'NaN') { price = '0.00'; }
+
+  return `ou em até ${args.installments} x ${args.currency} ${price}`;
 }
 
 export default helper(formatInstallments);
